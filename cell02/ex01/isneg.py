5472bb97d9bff89343ec0e7d33e1c1c0ba5f9c1b
PhitsiniBaitong <phitsini108137@gmail.com>
2025-06-17 05:01:02 +0000

    go

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+value = int(int())
+if value > 0:
+    print("This nember is positive.")
+elif value < 0:
+    print("This nember is negative.")
+else:
+    print("This number is both positive and negative.")
