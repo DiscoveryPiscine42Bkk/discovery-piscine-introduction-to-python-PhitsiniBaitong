ec4fbeb81bc9c4eb50d2d7eb8483de8030b9dc7d
PhitsiniBaitong <phitsini108137@gmail.com>
2025-06-17 06:16:35 +0000

    gdfgd

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,5 @@
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+array_double = [value * 2 for value in array]
+
+print(f"Original array: {array}")
+print(f"New array: {array_double}")
---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,5 @@
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+filtered_array = set([value for value in array if value > 5])
+
+print(array)
+print(filtered_array)
